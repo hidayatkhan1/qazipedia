0154422d7318a50568520c27580e356d2a83d9b9
hidayatkhan1 <hk9471295@gmail.com>
2026-08-17 12:01:04 +0500

    Add files via upload

---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHUl7IGv0gEp43fcJMZuB31TCXa2O8AHw",
+  authDomain: "qazipedia-cd938.firebaseapp.com",
+  projectId: "qazipedia-cd938",
+  storageBucket: "qazipedia-cd938.firebasestorage.app",
+  messagingSenderId: "273787299066",
+  appId: "1:273787299066:web:6aa97977e071290175d543",
+  measurementId: "G-G5Y6S1ERY5"
+};
+
